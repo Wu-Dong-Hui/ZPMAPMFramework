fde857bbcb2b074a5dc2b4252bc852c4b63fe160
wudonghui <donghui.wu@zhaopin.com.cn>
2018-04-10 16:29:31 +0800

    Initial Commit

---
+++ ZPMAPMDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZPMAPMDemo
+//
+//  Created by Roy on 2018/4/10.
+//  Copyright © 2018年 ZhaoPin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
